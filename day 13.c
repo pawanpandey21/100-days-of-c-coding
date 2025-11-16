45aaec2f7d73d5ffbcd90e3f6280af9dbac56a93
pawan kumar <Pawan.29505@stu.upes.ac.in>
2025-11-16 15:29:54 +0530

    Create day 13.c

---
+++ day 13.c
@@ -0,0 +1,58 @@
+
+/*Q25 (Conditional Statements)
+Write a program to implement a basic calculator using switch-case for +, -, *, /, %. */
+
+#include <stdio.h>
+
+int main(){
+    char op;
+    float num1, num2, result;
+    printf("Give calculator input (num1 num2 operator): \n");
+    scanf("%f %f %c", &num1, &num2, &op);
+    switch(op){
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2f \n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2f \n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2f \n", result);
+            break;
+        case '/':
+            if(num2 != 0){
+                result = num1 / num2;
+                printf("Result: %.2f \n", result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        case '%':
+            if((int)num2 != 0){
+                result = (int)num1 % (int)num2;
+                printf("Result: %f \n", result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        default:
+            printf("You can only use +, -, *, /, or %%.\n");
+    }
+}
+
+/* Q26 (Loops without Arrays/Strings)
+Write a program to print numbers from 1 to n. */
+
+#include <stdio.h>
+
+int main(){
+    int n, var = 1;
+    printf("Enter the value of n: \n");
+    scanf("%d", &n);
+    for (var=1; var<=n; var++){
+        printf("%d  ", var);
+    }
+}
