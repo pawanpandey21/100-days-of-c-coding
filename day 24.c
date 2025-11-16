90feab1159ee20664fd483ebb72006edf3c90c82
pawan kumar <Pawan.29505@stu.upes.ac.in>
2025-11-16 15:40:34 +0530

    Create day 24.c

---
+++ day 24.c
@@ -0,0 +1,45 @@
+
+/*Q47 (Nested Loops without Arrays/Strings)
+Write a program to print the following pattern:
+*
+**
+***
+****
+***** 
+*/
+
+#include <stdio.h>
+
+int main(){
+    for (int i=1; i<=5; i++){
+        for (int j=1; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/*Q48 (Nested Loops without Arrays/Strings)
+Write a program to print the following pattern:
+1
+12
+123
+1234
+12345
+*/
+
+#include <stdio.h>
+
+int main(){
+    int n=1;
+    for (int i=1; i<=5;i++){
+        for (int j=1; j<=i;j++){
+            printf("%d", n);
+            n+=1;
+        }
+        n = 1;
+        printf("\n");
+    }
+    return 0;
+}
