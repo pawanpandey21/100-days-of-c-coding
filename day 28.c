1a9ea575cb436397460f0583618066e1ed202330
pawan kumar <Pawan.29505@stu.upes.ac.in>
2025-11-16 15:44:22 +0530

    Create day 28.c

---
+++ day 28.c
@@ -0,0 +1,53 @@
+
+/*Q55 (Nested Loops without Arrays/Strings)
+Write a program to print all the prime numbers from 1 to n. */
+
+#include <stdio.h>
+
+int Prime(int n){
+    if (n <= 1) {
+        return 0;
+    }
+    int i=2;
+    while (i*i <=n){
+        if (n %i == 0){
+            return 0;
+        }
+        i++;
+    }
+        return 1;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Prime numbers from 1 to %d are: ", num);
+    for (int i = 1; i <= num; i++) {
+        if (Prime(i)) {
+            printf("%d ", i);
+        }
+    }
+}
+/* Q56 (Arrays (1D))
+Read and print elements of a one-dimensional array. */
+
+#include <stdio.h>
+
+int main(){
+    int len;
+    printf("Enter the length of the array: ");
+    scanf("%d", &len);
+
+    int arr[len];
+
+    printf("Enter elements: ");
+    for (int i=0; i<len; i++){    
+        scanf("%d", &arr[i]);
+    }
+
+    printf("The elements in the array are: ");
+    for (int i=0; i<len; i++){
+        printf("%d ", arr[i]);
+    }
+}
