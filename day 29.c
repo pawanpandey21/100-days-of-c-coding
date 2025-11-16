d70b47b63ef86698791b429d19e1eeaa886d44fa
pawan kumar <Pawan.29505@stu.upes.ac.in>
2025-11-16 15:47:45 +0530

    Create day 29.c

---
+++ day 29.c
@@ -0,0 +1,49 @@
+
+/*Q57 (Arrays (1D))
+Find the sum of array elements. */
+
+#include <stdio.h>
+
+int main(){
+    int n, sum=0;
+    printf("Enter the number of elements you want in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i=0; i<n;i++){
+        printf("Enter element number %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    for (int j=0; j<n; j++){
+        sum = sum + arr[j];
+    }
+    printf("The sum of the array: %d", sum);
+}
+/* Q58 (Arrays (1D))
+Find the maximum and minimum element in an array. */
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number of elements you want in the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i=0; i<n;i++){
+        printf("Enter element number %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0], min = arr[0];
+
+    for (int j = 0; j<n; j++){
+        if (arr[j] > max){
+            max = arr[j];
+        }
+
+        if (arr[j] < min){
+            min = arr[j];
+        }
+    }
+
+    printf("Max: %d\nMin: %d\n", max, min);
+}
